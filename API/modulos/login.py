2a656a733c622cbf9bd38efbec5d263b643fb8f7
MainDavis <44068483+MainDavis@users.noreply.github.com>
2022-05-01 17:26:16 +0200

    Eliminado cosas inecesarias

--- API/modulos/login.py
+++
@@ -1,9 +0,0 @@
-# Creo el modulo de login de Flask
-
-from flask import Blueprint
-
-login = Blueprint('login', __name__)
-
-@login.route('/')
-def index():
-    return "Hello from login"
--- API/modulos/register.py
+++
@@ -1,9 +0,0 @@
-# Creo el modulo de register de Flask
-
-from flask import Blueprint
-
-register = Blueprint('register', __name__)
-
-@register.route('/')
-def index():
-    return "Hello from register"
