a7b8fb108e1cd3dbd083f0f14e54c1942b6b485e
Arket19 <73239391+Arket19@users.noreply.github.com>
2022-05-24 12:52:35 +0200

    archivos html y css

---
+++ paginas_figma/cuenta_verificada/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+
+      <div class="titulo">
+        <h1 >Tu cuenta ha sido verificada.</h1>
+      </div>
+
+      <div>
+        <p class="subtitulo">Inicia sesión para acceder a la comunidad.</p>
+      </div>
+
+      <button class="boton">¡Vamos a ello!</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ paginas_figma/introducir_email/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div>
+      <div class="titulo">
+        <h1 >Estás a punto de formar parte de una gran<br />comunidad.</h1>
+      </div>
+
+      <div>
+        <p class="subtitulo">Introduce el correo electrónico de U-Tad</p>
+      </div>
+
+      <div>
+        <input type="mail" class="input-mail" placeholder="Correo electrónico"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
