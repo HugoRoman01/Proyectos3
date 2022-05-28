a85e06667b5463902a383ef14cf51f3ccab105bf
MainDavis <44068483+MainDavis@users.noreply.github.com>
2022-05-28 21:14:31 +0200

    css

--- app-deportes/src/componentes/Bienvenida/Bienvenida.jsx
+++ app-deportes/src/componentes/Bienvenida/Bienvenida.jsx
@@ -12,18 +12,18 @@ class Bienvenida extends React.Component {
     return (
     <div>
 
-      <div class="titulo">
+      <div class="titulo_bienvenida">
         <h1 >¡Bienvenido/a!</h1>
       </div>
 
       <div>
-        <p class="subtitulo"> La comunidad deportiva de U-Tad, donde podrás crear torneos y participar en eventos deportivos creados por los alumnos del centro.</p>
+        <p class="subtitulo_bienvenida"> La comunidad deportiva de U-Tad, donde podrás crear torneos y participar en eventos deportivos creados por los alumnos del centro.</p>
       </div>
 
-      <button class="boton" onClick={this.sendData}> <p class="txt_button">Acceder ahora</p> </button>
+      <button class="boton_bienvenida" onClick={this.sendData}> <p class="txt_button_bienvenida">Acceder ahora</p> </button>
 
-      <div class="imagen2-div"></div>
-      <div class="imagen1-div"></div>
+      <div class="imagen2-div_bienvenida"></div>
+      <div class="imagen1-div_bienvenida"></div>
     </div>
     )
   }
