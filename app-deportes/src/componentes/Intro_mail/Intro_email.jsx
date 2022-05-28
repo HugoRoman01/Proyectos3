49ec42bd1dc20ef789025c373bc9aa18fa88b997
Hugo Roman Dominguez <hromandomnguez69@gmail.com>
2022-05-28 21:22:54 +0200

    css

--- app-deportes/src/componentes/Intro_mail/Intro_email.jsx
+++ app-deportes/src/componentes/Intro_mail/Intro_email.jsx
@@ -7,16 +7,16 @@ function Intro_email() {
 
   return (
     <div>
-      <div class="titulo">
+      <div class="titulo_email">
         <h1 >Estás a punto de formar parte de una gran<br />comunidad.</h1>
       </div>
 
       <div>
-        <p class="subtitulo">Introduce el correo electrónico de U-Tad</p>
+        <p class="subtitulo_email">Introduce el correo electrónico de U-Tad</p>
       </div>
 
       <div>
-        <input type="mail" class="input-mail" placeholder="Correo electrónico"/>
+        <input type="mail" class="input-mail_email" placeholder="Correo electrónico"/>
       </div>
     </div>
   );
--- app-deportes/src/componentes/Link/Link_verificacion.jsx
+++ app-deportes/src/componentes/Link/Link_verificacion.jsx
@@ -6,17 +6,17 @@ function Link_verificacion() {
   return (
     <div>
 
-      <div class="titulo">
+      <div class="titulo_link">
         <h1 >Has recibido un link de verificación a </h1>
       </div>
 
       <div>
-        <p class="subtitulo">textodeprueba@live.u-tad.com</p>
+        <p class="subtitulo_link">textodeprueba@live.u-tad.com</p>
       </div>
 
-      <div class="conjunto">
-        <p class="llegado">¿No te ha llegado ningún correo?</p>
-        <p class="Reenviar">Reenviar.</p>
+      <div class="conjunto_link">
+        <p class="llegado_link">¿No te ha llegado ningún correo?</p>
+        <p class="Reenviar_link">Reenviar.</p>
       </div>
 
     </div>
