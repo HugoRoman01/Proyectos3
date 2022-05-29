190f66c800e46ddea308a5f74c100667371955e0
Hugo Roman Dominguez <hromandomnguez69@gmail.com>
2022-05-29 21:13:22 +0200

    Update App.js

--- app-deportes/src/App.js
+++ app-deportes/src/App.js
@@ -141,7 +141,7 @@ class App extends React.Component {
 
   render() {
 
-    switch ('register_email') {
+    switch (this.state.page) {
       case 'bienvenida':
         return ( <Bienvenida AppData={this.callbackFunction} onLoad={this.cookieLogin}/> );
 
