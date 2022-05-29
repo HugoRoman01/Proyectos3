bc48e9fb04b3c10faa8e1ba76031be9b61be5679
Arket19 <73239391+Arket19@users.noreply.github.com>
2022-05-30 00:40:30 +0200

    fecha fin implementada

--- app-deportes/src/componentes/CrearEvento3/CrearEvento3.jsx
+++ app-deportes/src/componentes/CrearEvento3/CrearEvento3.jsx
@@ -20,31 +20,29 @@ class CrearEvento3 extends React.Component {
       return (
 
         <div>
+          <div className="titulo_final">
+              <h1 >¿Cuándo?</h1>
+          </div>
 
-            <div className="titulo_final">
-                <h1 >¿Cuándo?</h1>
-            </div>
+          <div>
+              <p className="subtitulo_final">Proporciona una fecha para tu evento.</p>
+          </div>
 
-            <div>
-                <p className="subtitulo_final">Proporciona una fecha para tu evento.</p>
-            </div>
+          <p className="texto1">Del día</p>
+          <p className="texto4">al día</p>
+          <p className="texto2">desde las </p>
+          <p className="texto3">hasta las</p>
+          <p className="texto5">.</p>
 
-            <p className="texto1">El día</p>
-            <p className="texto2">de las </p>
-            <p className="texto3">a las</p>
-
-            <input type="date" id="fecha" value="" min="" max="" required></input>
-
-
-            <input type="time" id="hora_comienzo" min="08:00" max="21:00" required></input>
-            <input type="time" id="hora_fin" min="08:00" max="21:00" required></input>
-
-            <h1 className="flecha10">←</h1>
-            <button className="boton_creaevento4"> Guardar </button>
+          <input type="date" id="fecha" required></input>
+          <input type="date" id="fecha2" required></input>
 
 
-  
-  
+          <input type="time" id="hora_comienzo" min="08:00" max="21:00" required></input>
+          <input type="time" id="hora_fin" min="08:00" max="21:00" required/>
+
+          <h1 className="flecha10">←</h1>
+          <button className="boton_creaevento4"> Guardar </button>
         </div>
       )
     }
