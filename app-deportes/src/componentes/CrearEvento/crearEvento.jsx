356a6add57c4782610b295df9ad4e273e7797ba4
Arket19 <73239391+Arket19@users.noreply.github.com>
2022-05-29 18:07:30 +0200

    primeras dos

---
+++ app-deportes/src/componentes/CrearEvento/crearEvento.jsx
@@ -0,0 +1,34 @@
+import '../../estilos/crear_evento.css'
+import React from 'react'
+
+class Bienvenida extends React.Component {
+
+  
+  
+    render() {
+      
+      return (
+
+        <div>
+
+            <div class="titulo_crear_evento1">
+                <h1 >¿A qué quieres jugar?</h1>
+            </div>
+    
+            <select class="input_deporte">
+                <option value="Baloncesto">Baloncesto</option>
+                <option value="Fútbol">Fútbol</option>
+                <option value="Pádel">Pádel</option>
+            </select>
+  
+  
+  
+        </div>
+      )
+    }
+  
+  }
+  
+  
+  
+  export default Bienvenida;
