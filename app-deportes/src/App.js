5d254f0a51cf604516bd81fbf581580e77d1ff60
MainDavis <44068483+MainDavis@users.noreply.github.com>
2022-05-29 21:09:12 +0200

    Update App.js

--- app-deportes/src/App.js
+++ app-deportes/src/App.js
@@ -141,7 +141,7 @@ class App extends React.Component {
 
   render() {
 
-    switch (this.state.page) {
+    switch ('register_email') {
       case 'bienvenida':
         return ( <Bienvenida AppData={this.callbackFunction} onLoad={this.cookieLogin}/> );
 
